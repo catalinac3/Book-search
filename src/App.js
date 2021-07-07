5c41bbf0787a2141038ac686aab52817eb9765f1
catalina <cdianacatalina@gmail.com>
2021-07-07 23:25:10 +0200

    connected component searchField to App, passing state when a search is requested

--- src/App.js
+++ src/App.js
@@ -9,10 +9,15 @@ function App() {
     <OutputBook title={book.title} author={book.author} date={book.publishedDate}/>
   ));
 
+function search (x,y) {
+  alert("The form was submitted")
+  console.log(x,y);
+}
+
   return (
     <div className="App">
       <h1> BookFinder </h1>
-      <SearchField/>
+      <SearchField submitted={search}/>
       {BookList}
     </div>
   );
--- src/SearchField.js
+++ src/SearchField.js
@@ -1,39 +1,41 @@
 import React, { useState } from "react";
 
 function SearchField(props) {
-  // useState ({userSearch: ""}) is an array, the real state is in the 0 index,
-  // the second position in the array SetNewSearch is a function that can set the new state
-  const [Searched, setNewSearch] = useState({
-    userSearch: "",
-    selection: "title",
-  });
+  // useState ({userSearch: ""}) is an array, the real state (Searched) is in the 0 index,
+  // the second position in the array (SetNewSearch) is a function that can set the new state
+  const [Searched, setNewSearch] = useState({ userSearch: "" });
+  const [Selected, setNewSelection] = useState({ selection: "title" });
 
-  function inputChanged(event) {
-    setNewSearch({
-      userSearch: event.target.value,
-      selection: Searched.selection,
-    });
-  }
-  function selectionChanged(event) {
-    setNewSearch({
-      userSearch: Searched.userSearch,
-      selection: event.target.value,
-    });
-  }
-
+  // function submitted() {
+  //   alert("The form was submitted");
+  // }
+  console.log(props);
   return (
     <div>
-      <form>
-        <select onChange={selectionChanged}>
+      {/* */}
+      <form
+        onSubmit={(e) => {
+          //e.preventDefault avoids the page to reload on submit
+          e.preventDefault();
+          props.submitted(Searched.userSearch, Selected.selection);
+        }}
+      >
+        <select
+          onChange={(e) => setNewSelection({ selection: e.target.value })}
+        >
           <option defaultValue="title">title</option>
           <option value="author">author</option>
           <option value="date">date</option>
         </select>
-        <input type="text" onChange={inputChanged}></input>
+        <input
+          type="text"
+          onChange={(e) => setNewSearch({ userSearch: e.target.value })}
+        ></input>
         <input type="submit" value="Search"></input>
       </form>
-      <p>{Searched.userSearch}</p>
-      <p>{Searched.selection}</p>
+      <p>
+        {Searched.userSearch} {Selected.selection}
+      </p>
     </div>
   );
 }
